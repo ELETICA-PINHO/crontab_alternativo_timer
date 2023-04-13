6285109a8fb517123114f7e46cb19b23e5ff4df7
Alessandro Pinho <eletricapinho@gmail.com>
2023-04-13 15:29:21 -0300

    parte visual pronta com timer

--- crontab_alternativo_timer/Form1.cs
+++ crontab_alternativo_timer/Form1.cs
@@ -16,5 +16,70 @@ namespace crontab_alternativo_timer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtualizaHora();
+        }
+
+        private void AtualizaHora()
+        {
+            lblHoraAtual.Text = "Dia e Hora Atual:" + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AtualizaHora();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void btnRezetar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
